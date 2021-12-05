8fdd8e16620753c42b9e99e5b3596a25ec4fd80d
enshaeden <38257460+enshaeden@users.noreply.github.com>
2021-12-04 17:11:52 -0800

    Create boards.py

---
+++ boards.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+class dimensional_lumber():
+	def __init__(self, board_thickness, board_width):
+		self.board_width = board_width
+		self.board_thickness = board_thickness
+		
+	
+onebytwo = dimensional_lumber(1, 2)
+onebythree = dimensional_lumber(1, 3)
+onebyfour = dimensional_lumber(1, 4)
+twobyfour = dimensional_lumber(2, 4)
+twobysix = dimensional_lumber(2, 6)
+twobyeight = dimensional_lumber(2, 8)
+twobyten = dimensional_lumber(2, 10)
+twobytwelve = dimensional_lumber(2, 12)
+fourbyfour = dimensional_lumber(4, 4)
